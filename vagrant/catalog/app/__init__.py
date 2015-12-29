e46ccc791622ea59cc18260d27d3e68247879c53
Tomislav Pesut <tomca32@gmail.com>
2015-12-29 18:07:05 -0500

    Initialized catalog app

---
+++ vagrant/catalog/app/__init__.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+@app.errorhandler(404)
+def not_found(error):
+  return render_template('404.html'), 404
