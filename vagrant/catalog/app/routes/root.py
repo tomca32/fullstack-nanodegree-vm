2de589fff6ad20cf134f263e57c3ef320378f27a
Tomislav Pesut <tomca32@gmail.com>
2015-12-30 14:22:38 -0500

    Made root route render all categories

--- vagrant/catalog/app/routes/root.py
+++ vagrant/catalog/app/routes/root.py
@@ -1,6 +1,9 @@
 from flask import render_template
 from .. import app
+from .. import session
+from ..models import Category
 
 @app.route('/')
 def root():
-  return render_template('root.html')
+  categories = session.query(Category).all()
+  return render_template('root.html', categories = categories)
