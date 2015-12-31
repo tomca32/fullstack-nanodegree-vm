b8db799a2d9deca068dbf9f2f6e46ce3ebbc5360
Tomislav Pesut <tomca32@gmail.com>
2015-12-30 21:27:14 -0500

    Implemented adding new category

--- vagrant/catalog/app/routes/category.py
+++ vagrant/catalog/app/routes/category.py
@@ -1,6 +1,15 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from .. import app
+from .. import session
+from ..models import Category
 
-@app.route('/category/new')
-def newCategory():
+@app.route('/category/new', methods = ['GET'])
+def newCategoryForm():
   return render_template('category_new.html')
+
+@app.route('/category/new', methods = ['POST'])
+def newCategory():
+  newCategory = Category(name = request.form['name'])
+  session.add(newCategory)
+  session.commit()
+  return redirect(url_for('root'))
