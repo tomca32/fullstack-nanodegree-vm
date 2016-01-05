6fe55bc8a43e41380914fb4bc450781f74577e3f
Tomislav Pesut <tomca32@gmail.com>
2016-01-04 21:08:23 -0500

    Implemented guard against empty and whitespace item names

--- vagrant/catalog/app/decorators/__init__.py
+++ vagrant/catalog/app/decorators/__init__.py
@@ -1,4 +1,6 @@
-from flask import render_template
+import inspect
+
+from flask import render_template, request
 from sqlalchemy.orm.exc import NoResultFound
 
 from app.services import get_category_by_name, get_item_by_name_and_category_id
@@ -10,6 +12,7 @@ def with_category(f):
     :param f: view function or route being decorated
     :return: a decorator providing the category for the view function or rendering 404 if Category doesn't exist
     """
+
     def decorator(category_name, **kwargs):
         try:
             category = get_category_by_name(category_name)
@@ -17,7 +20,7 @@ def with_category(f):
             return render_template('404.html', message="Category '{0}' does not exist.".format(category_name))
         return f(category=category, **kwargs)
 
-    decorator.__name__ = f.__name__ # decorator gets the same name as the decorated function to preserve routing
+    decorator.__name__ = f.__name__  # decorator gets the same name as the decorated function to preserve routing
     return decorator
 
 
@@ -27,6 +30,7 @@ def with_item(f):
     :param f: view function or route being decorated
     :return: decorated providing the Item for the view function or rendering 404 if Category or Item don't exist
     """
+
     def decorator(category_name, item_name):
         try:
             category = get_category_by_name(category_name)
@@ -39,5 +43,25 @@ def with_item(f):
             return render_template('404.html', message="Item '{0}' does not exist in category {1].".format(item_name,
                                                                                                            category.name))
         return f(item=item)
-    decorator.__name__ = f.__name__ # decorator gets the same name as the decorated function to preserve routing
+
+    decorator.__name__ = f.__name__  # decorator gets the same name as the decorated function to preserve routing
+    return decorator
+
+
+def provide_query_args(f):
+    """
+    Provides arguments to the decorated function from query parameters if they match the function parameter names
+
+    :param f: Function to decorate
+    :return: Decorated function
+    """
+
+    def decorator():
+        args = request.args
+        # convert query arguments into a list of tuples
+        arguments = [(arg, args[arg]) for arg in inspect.getargspec(f).args if arg in args]
+        kwargs = dict((x, y) for x, y in arguments)  # convert list of tuple arguments in a dictionary
+        return f(**kwargs)
+
+    decorator.__name__ = f.__name__
     return decorator
--- vagrant/catalog/app/routes/item.py
+++ vagrant/catalog/app/routes/item.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for, flash
 
-from app.decorators import with_item
+from app.decorators import with_item, provide_query_args
 from .. import app
 from .. import session
 from ..models import Category
@@ -8,17 +8,31 @@ from ..services import item_exists, create_item, update_item, drop_item
 
 
 @app.route('/item/new', methods=['GET'])
-def new_item_form():
+@provide_query_args
+def new_item_form(default_name='', default_description='', default_category_name=''):
+    """
+    Displays a form for new item and also displays previous attempted fields if the form was invalid
+    :param default_name: Default name for the new item - displayed from the previous attempt if it failed
+    :param default_description: Default description - displayed from the previous attempt if it failed
+    :param default_category_name: Default category - displayed from the previous attempt if it failed
+    :return: rendered template item_new.html
+    """
     categories = session.query(Category).all()
-    return render_template('item_new.html', categories=categories)
+    return render_template('item_new.html', categories=categories, default_name=default_name,
+                           default_description=default_description,
+                           default_category_name=default_category_name)
 
 
 @app.route('/item/new', methods=['POST'])
 def new_item():
-    if item_exists(request.form['name']):
+    name = request.form['name'].strip()
+    if item_exists(name):
         flash('Error: Item with that name already exists')
-        return redirect(url_for('new_item_form'))
-    create_item(request.form['name'], request.form['description'], request.form['category'])
+        return redirect(retry_new_item(request.form))
+    if name == '':
+        flash('Error: Item must have a name')
+        return redirect(retry_new_item(request.form))
+    create_item(request.form['name'], request.form['description'].strip(), request.form['category'])
     return redirect(url_for('root'))
 
 
@@ -51,3 +65,8 @@ def delete_item_form(item):
 def delete_item(item_id):
     drop_item(item_id)
     return redirect(url_for('root'))
+
+
+def retry_new_item(form):
+    return url_for('new_item_form', default_name=form['name'], default_description=form['description'],
+                   default_category_name=form['category'])
