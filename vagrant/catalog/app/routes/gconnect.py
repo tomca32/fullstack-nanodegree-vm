36fd873d4fa8c46bdfc200040dcbba4d286ebe3c
Tomislav Pesut <tomca32@gmail.com>
2016-01-10 21:31:22 -0500

    Removed extra print

--- vagrant/catalog/app/routes/gconnect.py
+++ vagrant/catalog/app/routes/gconnect.py
@@ -99,8 +99,6 @@ def gdisconnect():
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
-    print 'result is '
-    print result
     if result['status'] == '200':
         del login_session['access_token']
         del login_session['gplus_id']
